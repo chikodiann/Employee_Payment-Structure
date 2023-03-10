b7b8942aa828bfa148afd0f920496663ef41a785
chikodiann <chikodiann@gmail.com>
2023-03-10 12:06:43 +0100

    another commit

--- src/main/java/org/example/SalariedEmployee.java
+++ src/main/java/org/example/SalariedEmployee.java
@@ -1,2 +1,21 @@
-package org.example;public class SalariedEmployee {
+package org.example;
+
+public class SalariedEmployee extends Employee{
+    private double annualSalary;
+    private boolean isRetired;
+
+    public SalariedEmployee(String name, String birthDate, String hireDate, double annualSalary) {
+        super(name, birthDate, hireDate);
+        this.annualSalary = annualSalary;
+    }
+@Override
+    public double collectPay() {
+        double paycheck = annualSalary / 26;
+        double adjustedPay = (isRetired) ? 0.9 * paycheck : paycheck;
+        return (int)adjustedPay;
+}
+public void retire() {
+        terminate("12/12/2023");
+        isRetired = true;
+}
 }
